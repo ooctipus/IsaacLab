9da70ae99a151a4a093686c251b825d4611711d7
Octi Zhang <zhengyuz@nvidia.com>
2025-07-23 22:30:40 -0700

    improved structure

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/dextrah/config/kuka_allegro/agents/rsl_rl_ppo_camera_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/dextrah/config/kuka_allegro/agents/rsl_rl_ppo_camera_cfg.py
@@ -6,12 +6,13 @@
 from isaaclab.utils import configclass
 
 from isaaclab_rl.rsl_rl import RslRlOnPolicyRunnerCfg, RslRlPpoActorCriticCfg, RslRlPpoAlgorithmCfg
+from isaaclab_rl.rsl_rl.actor_critic_vision_cfg import ActorCriticVisionAdapterCfg, CNNEncoderCfg
 
 @configclass
-class DexsuiteKukaAllegroPPORunnerCfg(RslRlOnPolicyRunnerCfg):
+class DexsuiteKukaAllegroPPORunnerCameraCfg(RslRlOnPolicyRunnerCfg):
     num_steps_per_env = 36
     obs_groups = {
-        "policy": ["policy", "privileged"],
+        "policy": ["policy", "base_image", "wrist_image"],
         "critic": ["policy", "privileged"]
     }
     max_iterations = 15000
@@ -24,6 +25,17 @@ class DexsuiteKukaAllegroPPORunnerCfg(RslRlOnPolicyRunnerCfg):
         actor_hidden_dims=[512, 256, 128],
         critic_hidden_dims=[512, 256, 128],
         activation="elu",
+        encoder=ActorCriticVisionAdapterCfg(
+            normalize=False,
+            encoder_cfg=CNNEncoderCfg(
+                channels=[32, 64, 128],
+                kernel_sizes=[3, 3, 3],
+                strides=[2, 2, 2],
+                paddings=[1, 1, 1],
+                use_maxpool=True,
+                pool_size=2
+            )
+        )
     )
     algorithm = RslRlPpoAlgorithmCfg(
         value_loss_coef=1.0,
