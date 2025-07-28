02388a26e4cc4e261ced7682f281753b7f3cb70e
Octi Zhang <zhengyuz@nvidia.com>
2025-07-28 13:12:20 -0700

    added shelves init

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/dextrah/config/kuka_allegro/__init__.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/dextrah/config/kuka_allegro/__init__.py
@@ -42,11 +42,11 @@ gym.register(
 )
 
 gym.register(
-    id="Dexsuite-Kuka-Allegro-Shelves-State-v0",
+    id="Dexsuite-Kuka-Allegro-Shelves-Reorient-State-v0",
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": f"{__name__}.dexsuite_kuka_allegro_env_cfg:DexsuiteKukaAllegroShelvesEnvCfg",
+        "env_cfg_entry_point": f"{__name__}.dexsuite_kuka_allegro_env_cfg:DexsuiteKukaAllegroShelvesReorientEnvCfg",
         "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_pbt_cfg.yaml",
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:DexsuiteKukaAllegroPPORunnerCfg",
         "configurable_entry_point": f"{__name__}.configurables:EnvConfigurables",
@@ -54,11 +54,35 @@ gym.register(
 )
 
 gym.register(
-    id="Dexsuite-Kuka-Allegro-Shelves-State-Play-v0",
+    id="Dexsuite-Kuka-Allegro-Shelves-Reorient-State-Play-v0",
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": f"{__name__}.dexsuite_kuka_allegro_env_cfg:DexsuiteKukaAllegroReorientEnvCfg_PLAY",
+        "env_cfg_entry_point": f"{__name__}.dexsuite_kuka_allegro_env_cfg:DexsuiteKukaAllegroShelvesReorientEnvCfg_PLAY",
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_pbt_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:DexsuiteKukaAllegroPPORunnerCameraCfg",
+        "configurable_entry_point": f"{__name__}.configurables:EnvConfigurables",
+    },
+)
+
+gym.register(
+    id="Dexsuite-Kuka-Allegro-Shelves-Place-State-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.dexsuite_kuka_allegro_env_cfg:DexsuiteKukaAllegroShelvesPlaceEnvCfg",
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_pbt_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:DexsuiteKukaAllegroPPORunnerCfg",
+        "configurable_entry_point": f"{__name__}.configurables:EnvConfigurables",
+    },
+)
+
+gym.register(
+    id="Dexsuite-Kuka-Allegro-Shelves-Place-State-Play-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.dexsuite_kuka_allegro_env_cfg:DexsuiteKukaAllegroShelvesPlaceEnvCfg_PLAY",
         "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_pbt_cfg.yaml",
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:DexsuiteKukaAllegroPPORunnerCameraCfg",
         "configurable_entry_point": f"{__name__}.configurables:EnvConfigurables",
