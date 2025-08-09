f7cbf754c55b2b88de35b3b24e35ed516595e89a
Octi Zhang <zhengyuz@nvidia.com>
2025-08-08 23:29:12 -0700

    add uitility to main

---
+++ buildnpush.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <tag>"
+  exit 1
+fi
+
+TAG="$1"
+BASE_IMAGE="isaac-lab-base"
+FINAL_IMAGE="nvcr.io/nvidian/octi-isaac-lab:${TAG}"
+
+echo "▶️  Starting container service"
+./docker/container.py start
+
+echo "🏷  Tagging image as ${FINAL_IMAGE}"
+docker tag "${BASE_IMAGE}" "${FINAL_IMAGE}"
+
+echo "📤 Pushing to NGC: ${FINAL_IMAGE}"
+/home/zhengyuz/ngc-cli/ngc registry image push "${FINAL_IMAGE}"
+echo "📤 Image: ${FINAL_IMAGE} pushed to NGC: "
+
+echo "✅ Done!"
