61291848bcb088c31997dc923a778f6c966d4748
Octi Zhang <zhengyuz@nvidia.com>
2025-08-15 12:57:05 -0700

    add fabric as task id rather than hydra config

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/dextrah/config/kuka_allegro/__init__.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/dextrah/config/kuka_allegro/__init__.py
@@ -111,6 +111,32 @@ gym.register(
 )
 
 gym.register(
+    id="Dextrah-Fabric-Kuka-Allegro-Lift-State-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.dextrah_kuka_allegro_env_cfg:DextrahFabricKukaAllegroLiftEnvCfg",
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_pbt_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:DextrahKukaAllegroPPORunnerCfg",
+        "configurable_entry_point": f"{__name__}.configurables:EnvConfigurables",
+    },
+)
+
+
+gym.register(
+    id="Dextrah-Fabric-Kuka-Allegro-Lift-State-Play-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.dextrah_kuka_allegro_env_cfg:DextrahFabricKukaAllegroLiftEnvCfg_PLAY",
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_pbt_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:DextrahKukaAllegroPPORunnerCfg",
+        "configurable_entry_point": f"{__name__}.configurables:EnvConfigurables",
+    },
+)
+
+
+gym.register(
     id="Dextrah-Kuka-Allegro-Lift-State-Play-v0",
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
