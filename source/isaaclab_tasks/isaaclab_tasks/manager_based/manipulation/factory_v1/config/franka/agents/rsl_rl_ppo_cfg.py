8656848b6f5002d109c70747496319d46f017a9c
Octi Zhang <zhengyuz@nvidia.com>
2025-08-22 17:20:26 -0700

    increase iterations max count

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/factory_v1/config/franka/agents/rsl_rl_ppo_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/factory_v1/config/franka/agents/rsl_rl_ppo_cfg.py
@@ -11,7 +11,7 @@ from isaaclab_rl.rsl_rl import RslRlOnPolicyRunnerCfg, RslRlPpoActorCriticCfg, R
 @configclass
 class FactoryPPORunnerCfg(RslRlOnPolicyRunnerCfg):
     num_steps_per_env = 32
-    max_iterations = 1500
+    max_iterations = 5000
     save_interval = 50
     experiment_name = "factory"
     empirical_normalization = True
