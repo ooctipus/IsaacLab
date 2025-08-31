e449f61b220d3d7f535f2016452c484ab29d3eb0
Octi Zhang <zhengyuz@nvidia.com>
2025-08-31 14:16:59 -0700

    default to 8 seconds

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/factory_v1/factory_env_base.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/factory_v1/factory_env_base.py
@@ -260,7 +260,7 @@ class FactoryBaseEnvCfg(ManagerBasedRLEnvCfg):
         """Post initialization."""
         # general settings
         self.decimation = 12
-        self.episode_length_s = 5.0
+        self.episode_length_s = 8.0
         # simulation settings
         self.sim.dt = 0.1 / self.decimation
         self.sim.render_interval = self.decimation
