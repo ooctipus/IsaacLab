0159a7c414a31af9a320a8d7436669d4a70808f4
Octi Zhang <zhengyuz@nvidia.com>
2025-09-02 01:55:32 -0700

    dexsuite pre release

---
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/dextrah/adr_curriculum.py
@@ -0,0 +1,108 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+from isaaclab.managers import CurriculumTermCfg as CurrTerm
+from isaaclab.utils import configclass
+
+from . import mdp
+
+
+@configclass
+class CurriculumCfg:
+    """Curriculum terms for the MDP."""
+
+    adr = CurrTerm(
+        func=mdp.DifficultyScheduler, params={"init_difficulty": 0, "min_difficulty": 0, "max_difficulty": 10}
+    )
+
+    joint_pos_unoise_min_adr = CurrTerm(
+        func=mdp.modify_term_cfg,
+        params={
+            "address": "observations.policy.joint_pos.noise.n_min",
+            "modify_fn": mdp.initial_final_interpolate_fn,
+            "modify_params": {"iv": 0., "fv": -.1, "difficulty_term_str": "adr"}
+        }
+    )
+
+    joint_pos_unoise_max_adr = CurrTerm(
+        func=mdp.modify_term_cfg,
+        params={
+            "address": "observations.policy.joint_pos.noise.n_max",
+            "modify_fn": mdp.initial_final_interpolate_fn,
+            "modify_params": {"iv": 0., "fv": .1, "difficulty_term_str": "adr"}
+        }
+    )
+
+    joint_vel_unoise_min_adr = CurrTerm(
+        func=mdp.modify_term_cfg,
+        params={
+            "address": "observations.policy.joint_vel.noise.n_min",
+            "modify_fn": mdp.initial_final_interpolate_fn,
+            "modify_params": {"iv": 0., "fv": -.2, "difficulty_term_str": "adr"}
+        }
+    )
+
+    joint_vel_unoise_max_adr = CurrTerm(
+        func=mdp.modify_term_cfg,
+        params={
+            "address": "observations.policy.joint_vel.noise.n_max",
+            "modify_fn": mdp.initial_final_interpolate_fn,
+            "modify_params": {"iv": 0., "fv": .2, "difficulty_term_str": "adr"}
+        }
+    )
+
+    hand_tips_pos_unoise_min_adr = CurrTerm(
+        func=mdp.modify_term_cfg,
+        params={
+            "address": "observations.policy.hand_tips_state_b.noise.n_min",
+            "modify_fn": mdp.initial_final_interpolate_fn,
+            "modify_params": {"iv": 0., "fv": -.01, "difficulty_term_str": "adr"}
+        }
+    )
+
+    hand_tips_pos_unoise_max_adr = CurrTerm(
+        func=mdp.modify_term_cfg,
+        params={
+            "address": "observations.policy.hand_tips_state_b.noise.n_max",
+            "modify_fn": mdp.initial_final_interpolate_fn,
+            "modify_params": {"iv": 0., "fv": .01, "difficulty_term_str": "adr"}
+        }
+    )
+
+    object_quat_unoise_min_adr = CurrTerm(
+        func=mdp.modify_term_cfg,
+        params={
+            "address": "observations.policy.object_quat_b.noise.n_min",
+            "modify_fn": mdp.initial_final_interpolate_fn,
+            "modify_params": {"iv": 0., "fv": -.03, "difficulty_term_str": "adr"}
+        }
+    )
+
+    object_quat_unoise_max_adr = CurrTerm(
+        func=mdp.modify_term_cfg,
+        params={
+            "address": "observations.policy.object_quat_b.noise.n_max",
+            "modify_fn": mdp.initial_final_interpolate_fn,
+            "modify_params": {"iv": 0., "fv": .03, "difficulty_term_str": "adr"}
+        }
+    )
+
+    object_obs_unoise_min_adr = CurrTerm(
+        func=mdp.modify_term_cfg,
+        params={
+            "address": "observations.privileged.perception.noise.n_min",
+            "modify_fn": mdp.initial_final_interpolate_fn,
+            "modify_params": {"iv": 0., "fv": -.01, "difficulty_term_str": "adr"}
+        }
+    )
+
+    object_obs_unoise_max_adr = CurrTerm(
+        func=mdp.modify_term_cfg,
+        params={
+            "address": "observations.privileged.perception.noise.n_max",
+            "modify_fn": mdp.initial_final_interpolate_fn,
+            "modify_params": {"iv": 0., "fv": -.01, "difficulty_term_str": "adr"}
+        }
+    )
---
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/dextrah/config/kuka_allegro/__init__.py
@@ -0,0 +1,63 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""
+Dextra Kuka Allegro environments.
+"""
+
+import gymnasium as gym
+
+from . import agents
+
+##
+# Register Gym environments.
+##
+
+# State Observation
+gym.register(
+    id="Dextrah-Kuka-Allegro-Reorient-State-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.dextrah_kuka_allegro_env_cfg:DextrahKukaAllegroReorientEnvCfg",
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:DextrahKukaAllegroPPORunnerCfg",
+    },
+)
+
+gym.register(
+    id="Dextrah-Kuka-Allegro-Reorient-State-Play-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.dextrah_kuka_allegro_env_cfg:DextrahKukaAllegroReorientEnvCfg_PLAY",
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:DextrahKukaAllegroPPORunnerCameraCfg"
+    },
+)
+
+# Dextrah Lift Environments
+gym.register(
+    id="Dextrah-Kuka-Allegro-Lift-State-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.dextrah_kuka_allegro_env_cfg:DextrahKukaAllegroLiftEnvCfg",
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:DextrahKukaAllegroPPORunnerCfg"
+    },
+)
+
+
+gym.register(
+    id="Dextrah-Kuka-Allegro-Lift-State-Play-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.dextrah_kuka_allegro_env_cfg:DextrahKukaAllegroLiftEnvCfg_PLAY",
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:DextrahKukaAllegroPPORunnerCfg"
+    },
+)
